480de1ceb57826a5fbc7cce72ac39d7d60d6dcf1
Syafwan000 <muhammadsyafwan04@gmail.com>
2025-06-10 09:36:55 +0700

    feat: add user admin as default access to admin panel

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -5,6 +5,7 @@ namespace Database\Seeders;
 use App\Models\User;
 // use Illuminate\Database\Console\Seeds\WithoutModelEvents;
 use Illuminate\Database\Seeder;
+use Illuminate\Support\Facades\Hash;
 
 class DatabaseSeeder extends Seeder
 {
@@ -13,6 +14,11 @@ class DatabaseSeeder extends Seeder
      */
     public function run(): void
     {
+        User::create([
+            'username' => 'admin',
+            'password' => Hash::make('password')
+        ]);
+
         User::factory(10)->create();
 
         // User::factory()->create([
